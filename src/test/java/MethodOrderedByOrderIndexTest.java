da25645d77b8da9167ee2ebd7bcc5c0677afc39e
Manjiri Chandure <manjirichandure@Manjiri-ka-MacBook-Pro.local>
2025-07-14 15:05:50 +0530

    Added method ordered by Order index

---
+++ src/test/java/MethodOrderedByOrderIndexTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderedByOrderIndexTest {
+
+    @Order(3)
+    @Test
+    void testA(){
+        System.out.println("Running test A");
+    }
+
+    @Order(1)
+    @Test
+    void testB(){
+        System.out.println("Running test B");
+    }
+
+    @Order(2)
+    @Test
+    void testC(){
+        System.out.println("Running test C");
+    }
+
+    @Order(4)
+    @Test
+    void testD(){
+        System.out.println("Running test D");
+    }
+
+}
